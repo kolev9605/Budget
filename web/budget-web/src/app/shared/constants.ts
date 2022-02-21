8ce3683e3833cb11baface623507b0809a25f273
Georgi Kolev <kolev9605@gmail.com>
2022-02-21 15:30:09 +0200

    Handle errors vie errorService; cleanup

--- web/budget-web/src/app/shared/constants.ts
+++ web/budget-web/src/app/shared/constants.ts
@@ -1,8 +1,3 @@
 export const ColorConstants = {
   BACKGROUND: '#d8d8d8',
 };
-
-export const CommonConstants = {
-  SIGN_IN_FORM_TYPE: 'signIn',
-  SIGN_UP_FORM_TYPE: 'signUp',
-};
--- web/budget-web/src/app/shared/services/auth.service.ts
+++ web/budget-web/src/app/shared/services/auth.service.ts
@@ -4,19 +4,26 @@ import { environment } from '../../../environments/environment';
 import { RegisterModel } from '../models/authentication/register.model';
 import { LoginModel } from '../models/authentication/login.model';
 import { TokenModel } from '../models/authentication/token.model';
+import { ErrorService } from './error.service';
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
 
-  register(signUpModel: RegisterModel) {
-    return this.http.post(environment.apiUrl + 'Authentication/Register', signUpModel);
+  register(signUpModel: RegisterModel): Observable<Object> {
+    return this.http
+      .post(environment.apiUrl + 'Authentication/Register', signUpModel)
+      .pipe(catchError(this.errorService.handleError));
   }
 
-  login(signUpModel: LoginModel) {
-    return this.http.post<TokenModel>(environment.apiUrl + 'Authentication/Login', signUpModel);
+  login(signUpModel: LoginModel): Observable<TokenModel> {
+    return this.http
+      .post<TokenModel>(environment.apiUrl + 'Authentication/Login', signUpModel)
+      .pipe(catchError(this.errorService.handleError));
   }
 
   signOut() {}
