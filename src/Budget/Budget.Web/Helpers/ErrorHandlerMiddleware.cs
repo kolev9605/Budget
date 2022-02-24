95cda401c9014a5af8f43f9c99866fa8800d9347
Georgi Kolev <kolev9605@gmail.com>
2022-02-24 02:10:23 +0200

    Basic Serilog Setup

--- src/Budget/Budget.Web/Helpers/ErrorHandlerMiddleware.cs
+++ src/Budget/Budget.Web/Helpers/ErrorHandlerMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Budget.Core.Exceptions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Net;
 using System.Text.Json;
@@ -10,10 +11,12 @@ namespace Budget.Web.Helpers
     public class ErrorHandlerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
 
-        public ErrorHandlerMiddleware(RequestDelegate next)
+        public ErrorHandlerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
         {
             _next = next;
+            _logger = loggerFactory.CreateLogger<ErrorHandlerMiddleware>();
         }
 
         public async Task Invoke(HttpContext context)
@@ -29,16 +32,18 @@ namespace Budget.Web.Helpers
 
                 switch (error)
                 {
+                    // TODO: take advantage of the templates and log more detailed information about the error
                     case BudgetValidationException budgetValidationException:
-                        // custom application error
                         response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        _logger.LogWarning("{exception}", error);
                         break;
                     case BudgetAuthenticationException budgetAuthorizationException:
                         response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        _logger.LogWarning("{exception}", error);
                         break;
                     default:
-                        // unhandled error
                         response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        _logger.LogError("{exception}", error);
                         break;
                 }
 
