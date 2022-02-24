90cbe057a611fbcf2525f1eb70199175d0428883
Georgi Kolev <kolev9605@gmail.com>
2022-02-24 17:54:49 +0200

    AuthGuard implemented

--- web/budget-web/src/app/app-routing.module.ts
+++ web/budget-web/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './shared/services/auth.guard';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'sign-in', component: AuthenticationComponent },
 ];
 
