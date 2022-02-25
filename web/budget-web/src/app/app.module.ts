4dad170ca11f935f35bc16797b6389218887373e
Georgi Kolev <kolev9605@gmail.com>
2022-02-25 12:59:30 +0200

    FormWrapperComponent declared in app.module.ts

--- web/budget-web/src/app/app.module.ts
+++ web/budget-web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoadingSpinnerComponent } from './shared/components/loading-spinner/loading-spinner.component';
 import { AuthInterceptor } from './shared/services/auth.interceptor';
 import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { FormWrapperComponent } from './shared/components/form-wrapper/form-wrapper.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { CreateAccountComponent } from './account/create-account/create-account.
     AuthenticationComponent,
     LoadingSpinnerComponent,
     CreateAccountComponent,
+    FormWrapperComponent,
   ],
   imports: [
     BrowserModule,
