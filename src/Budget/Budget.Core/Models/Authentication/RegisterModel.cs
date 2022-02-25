7a48a9eb4bd49beac21a638447eb360c1bd482ab
Georgi Kolev <kolev9605@gmail.com>
2022-02-25 23:28:56 +0200

    ToLoginModel not needed

--- src/Budget/Budget.Core/Models/Authentication/RegisterModel.cs
+++ src/Budget/Budget.Core/Models/Authentication/RegisterModel.cs
@@ -13,14 +13,5 @@ namespace Budget.Core.Models.Authentication
 
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
-
-        public LoginModel ToLoginModel()
-        {
-            return new LoginModel
-            {
-                Username = Username,
-                Password = Password,
-            };
-        }
     }
 }
