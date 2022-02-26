2d12f491c9e3759316e9a134cccc2f937359d19e
Georgi Kolev <kolev9605@gmail.com>
2022-02-27 00:07:34 +0200

    Remove attribute validation

--- src/Budget/Budget.Core/Models/Authentication/LoginModel.cs
+++ src/Budget/Budget.Core/Models/Authentication/LoginModel.cs
@@ -4,10 +4,8 @@ namespace Budget.Core.Models.Authentication
 {
     public class LoginModel
     {
-        [Required(ErrorMessage = "User Name is required")]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
-        public string? Password { get; set; }
+        public string Password { get; set; }
     }
 }
