6d9fdf8835acb88de6423c41ea7f934aca050fe2
Georgi Kolev <kolev9605@gmail.com>
2022-03-05 12:55:34 +0200

    Add Helpers class in Common Project

---
+++ src/Budget/Budget.Common/Helpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Budget.Common
+{
+    public static class Helpers
+    {
+        public static IEnumerable<string> GetListFromEnum<T>()
+            where T : struct, IConvertible
+        {
+            var type = typeof(T);
+
+            if (!type.IsEnum)
+            {
+                throw new ArgumentException("Type T must be an enum type.");
+            }
+
+            return Enum.GetNames(type);
+        }
+    }
+}
