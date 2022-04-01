f2d6132f06e66258eb36a6738fc406f504ba5264
Georgi Kolev <kolev9605@gmail.com>
2022-04-01 19:36:32 +0300

    Accounts page

--- web/budget-web/src/app/accounts/accounts.component.ts
+++ web/budget-web/src/app/accounts/accounts.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { tap } from 'rxjs/operators';
+import { AccountModel } from '../shared/models/accounts/account.model';
+import { AccountService } from '../shared/services/account.service';
 
 @Component({
   selector: 'app-accounts',
@@ -6,7 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accounts.component.scss'],
 })
 export class AccountsComponent implements OnInit {
-  constructor() {}
+  isLoading: boolean;
+  accounts: AccountModel[];
 
-  ngOnInit(): void {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.accountService.getAll().subscribe(
+      (response) => {
+        this.isLoading = false;
+        this.accounts = response;
+      },
+      (error) => {
+        this.isLoading = false;
+        this.toastr.error(error);
+      },
+    );
+  }
 }
