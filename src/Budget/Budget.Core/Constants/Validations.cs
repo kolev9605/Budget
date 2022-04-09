c83b595efd4542b98baaa0d08f305954e2cb700f
Georgi Kolev <kolev9605@gmail.com>
2022-04-10 01:11:18 +0300

    Validations in constants

---
+++ src/Budget/Budget.Core/Constants/Validations.cs
@@ -0,0 +1,10 @@
+﻿namespace Budget.Core.Constants
+{
+    public static class Validations
+    {
+        public static class Records
+        {
+            public const int NoteMaxLength = 100;
+        }
+    }
+}
