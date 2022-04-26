584e72bfd1ebb9d3aa439fa1eb2a9c96ee1a1a03
Georgi Kolev <kolev9605@gmail.com>
2022-04-26 23:50:26 +0300

    Helper DateTimeExtensions

---
+++ src/Budget/Budget.Common/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Budget.Common
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime? SetKindUtc(this DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+            {
+                return dateTime.Value.SetKindUtc();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static DateTime SetKindUtc(this DateTime dateTime)
+        {
+            if (dateTime.Kind == DateTimeKind.Utc) 
+            { 
+                return dateTime; 
+            }
+
+            return DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+        }
+    }
+}
