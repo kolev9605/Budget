7315ad92ce564d1140cc2dd85c2e0043a0ea3734
Georgi Kolev <kolev9605@gmail.com>
2022-05-12 01:14:44 +0300

    Test controller

---
+++ src/Budget/Budget.Web/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Budget.Web.Controllers
+{
+    [AllowAnonymous]
+    [Route("Statistics")]
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        [Route(nameof(Hello))]
+        public IActionResult Hello()
+            => Ok("I am working");
+    }
+}
