9fc543e1385510a10a9b5ffaaa58c5c54c893006
Georgi Kolev <kolev9605@gmail.com>
2023-09-21 17:55:45 +0300

    Budget.Web -> Budget.Api

---
+++ src/Budget/Budget.Api/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Budget.Api.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public abstract class BaseController : ControllerBase
+{
+    protected string? LoggedInUserId
+    {
+        get
+        {
+            return User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
---
+++ src/Budget/Budget.Api/Controllers/ChartController.cs
@@ -0,0 +1,22 @@
+﻿using Budget.Domain.Interfaces.Services;
+using Budget.Domain.Models.Charts.CashFlow;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace Budget.Api.Controllers;
+
+public class ChartController : BaseController
+{
+    private readonly IChartService _chartService;
+
+    public ChartController(IChartService chartService)
+    {
+        _chartService = chartService;
+    }
+
+    [HttpPost]
+    [Route(nameof(GetCashFlowData))]
+    public async Task<IActionResult> GetCashFlowData(CashFlowChartRequestModel cashFlowChartRequestModel)
+        => Ok(await _chartService.GetCashFlowChartDataAsync(cashFlowChartRequestModel, LoggedInUserId));
+}
