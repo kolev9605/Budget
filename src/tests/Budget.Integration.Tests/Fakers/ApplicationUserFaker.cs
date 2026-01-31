bf25b05f9221a9d45bd552bd79d5339d78e667d9
Georgi Kolev <kolev9605@gmail.com>
2026-02-01 01:30:50 +0200

    test: Add rule for generating unique Id in ApplicationUserFaker

--- src/tests/Budget.Integration.Tests/Fakers/ApplicationUserFaker.cs
+++ src/tests/Budget.Integration.Tests/Fakers/ApplicationUserFaker.cs
@@ -7,6 +7,7 @@ public class ApplicationUserFaker : Faker<ApplicationUser>
 {
     public ApplicationUserFaker()
     {
+        RuleFor(u => u.Id, f => Guid.NewGuid().ToString());
         RuleFor(u => u.UserName, f => f.Internet.UserName());
         RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.UserName));
         RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber());
